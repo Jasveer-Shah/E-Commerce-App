b49a4ee67c0fe0019a7e443594ad833fc6ef5ce5
jasveer shah <talkjasveer@gmail.com>
2021-08-08 12:44:24 -0400

    starter files

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Background from "./Background";
+
+import Cart from './Cart';
+import Header from './Header';
+import Home from './Home';
+import Browse from './Browse';
+
+export { Background, Cart, Header, Home, Browse }
